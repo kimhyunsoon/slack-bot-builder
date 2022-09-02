75ece4d75c2ca32059b66d21221daf5435d8f94b
soon <sudosoon@gmail.com>
2022-09-02 21:34:14 +0900

    wrap in a docker

---
+++ src/lunch.js
@@ -0,0 +1,99 @@
+import { WebClient } from '@slack/web-api';
+import schedule from 'node-schedule';
+import moment from 'moment';
+import slack from './slack-key.js'
+
+const web = new WebClient(slack.lunch.bot_token);
+
+const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+const rand = (max) => {
+  return Math.floor(Math.random() * (max - 1));
+}
+
+const log = {
+  error: (msg) => {
+    let date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+    console.error(`#error::${date} ${days[moment().day()]}요일 [ ${msg} ]`);
+  },
+  info: (msg) => {
+    let date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+    console.log(`#info::${date} ${days[moment().day()]}요일 [ ${msg} ]`);
+  }
+};
+
+const list = [
+  { location: '신송빌딩', far: 389, name: '돈대리돈까스백반', category: '한식돈까스' },
+  { location: '신영증권', far: 796, name: '하카타분코', category: '라멘' },
+  { location: '여의도백화점', far: 698, name: '양푼집 흑돼지김치찌개', category: '김치찌개' },
+  { location: 'KTB빌딩', far: 330, name: '오한수 우육면가', category: '우육면' },
+  { location: '신송빌딩', far: 389, name: '지리산명가', category: '백반' },
+  { location: '신송빌딩', far: 389, name: '사계절집밥', category: '백반' },
+  { location: '대오빌딩', far: 435, name: '명동칼국수', category: '칼국수' },
+  { location: '대오빌딩', far: 435, name: '대문집', category: '한식' },
+  { location: '유성빌딩', far: 844, name: '청키면가', category: '완탕면' },
+  { location: 'IFC', far: 177, name: 'CJ더마켓', category: '샐러드' },
+  { location: 'IFC', far: 108, name: '맥도날드', category: '햄버거' },
+  { location: 'IFC', far: 108, name: '판다익스프레스', category: '중식' },
+  { location: '신송빌딩', far: 389, name: '다다미생선구이', category: '생선구이' },
+  { location: 'KTB빌딩', far: 330, name: '오늘은분식', category: '분식' },
+  { location: 'BNK금융타워', far: 261, name: '진순대', category: '순대국' },
+  { location: '여의도백화점', far: 698, name: '보글보글부대찌개', category: '부대찌개' },
+  { location: '동화빌딩', far: 288, name: '더차이니스', category: '중식' },
+  { location: '율촌빌딩', far: 290, name: '바스버거', category: '햄버거' },
+  { location: '메리츠화재여의도사옥', far: 293, name: '사보텐', category: '일식돈까스우동' },
+  { location: '씨티플라자', far: 252, name: '완백부대찌개', category: '부대찌개' },
+  { location: 'IFC', far: 108, name: '남기분면', category: '홍콩쌀국수' },
+  { location: 'S-Trenue', far: 371, name: '난바우동', category: '우동' },
+  { location: '포스트타워', far: 432, name: '제주은희네해장국', category: '해장국' },
+  { location: 'S-Trenue', far: 371, name: '와인주막차차', category: '퓨전한식' },
+  { location: 'S-Trenue', far: 371, name: '미미쌀국수', category: '쌀국수' },
+  { location: '파이낸스타워', far: 310, name: '사위식당', category: '낙곱새' },
+  { location: 'IFC', far: 108, name: '히클리밥', category: '한상밥집' },
+];
+
+let lastNumbers = [];
+
+const getLastNumbers = async () => {
+  lastNumbers = [];
+  const history = await getHistory();
+  const filter = history.messages.filter((r) => r.bot_id === slack.lunch.bot_id)
+    .slice(0, 10).reduce((acc, r) => {
+      acc.push(r.text.split('\n')[1].split(' / ')[0]);
+      return acc;
+    }, []);
+  
+  list.forEach((r, i) => {
+    if (filter.indexOf(r.name) !== -1) lastNumbers.push(i);  
+  })
+}
+
+const getNumber = () => {
+  const num = rand(list.length);
+  if (lastNumbers.indexOf(num) !== -1) return getNumber();
+  return num;
+}
+const getMenu = async () => {
+  await getLastNumbers();
+  const num = await getNumber();
+  return list[num];
+}
+
+const getHistory = async () => {
+  const history = await web.conversations.history({
+    channel: slack.lunch.channel,
+  });
+  // console.log(history);
+  return history;
+}
+
+const scheduledr = schedule.scheduleJob('0 0 11 * * MON-FRI', async () => {
+  const menu = await getMenu();
+  const date = `${moment(new Date()).format('MM월 DD일')} ${days[moment().day()]}요일`;
+  web.chat.postMessage({
+    channel: slack.lunch.channel,
+    text: `${date} 추천 메뉴는 '${menu.category}' 입니다.\n${menu.name} / ${menu.location}(${menu.far}m)`,
+    as_user: true
+  });
+  log.info(`${menu.name} / ${menu.category} / ${menu.location}(${menu.far})`);
+});
---
+++ src/stock.js
@@ -0,0 +1,410 @@
+import { WebClient } from '@slack/web-api';
+import schedule from 'node-schedule';
+import moment from 'moment';
+import cheerio from 'cheerio';
+import axios from "axios";
+import slack from './slack-key.js';
+import ChartJSImage from 'chart.js-image';
+import { createReadStream } from 'fs';
+
+const web = new WebClient(slack.stock.bot_token);
+
+const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+const log = {
+  error: (msg) => {
+    let date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+    console.error(`#error::${date} ${days[moment().day()]}요일 [ ${msg} ]`);
+  },
+  info: (msg) => {
+    let date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+    console.log(`#info::${date} ${days[moment().day()]}요일 [ ${msg} ]`);
+  }
+};
+
+const priceToString = (price) => {
+  return `${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원`;
+}; 
+
+const data = [
+  {
+    name: 'CMA계좌',
+    stocks: [
+      {
+        code: '005930',
+        name: '삼성전자',
+        average: 72500,
+        cnt: 20,
+      },
+      {
+        code: '005930',
+        name: '삼성전자(고모)',
+        average: 57500,
+        cnt: 18,
+      },
+    ]
+  },
+  {
+    name: '연금저축계좌',
+    stocks: [
+      {
+        code: '133690',
+        name: 'TIGER 미국나스닥100',
+        average: 67605,
+        cnt: 103,
+      },
+      {
+        code: '360750',
+        name: 'TIGER 미국S&P500',
+        average: 11485,
+        cnt: 545,
+      },
+    ]
+  },
+]
+const getPresentPrice = async (code) => {
+  try {
+    const html = await axios.get(`https://finance.naver.com/item/main.naver?code=${code}`);
+    const $ = cheerio.load(html.data);
+    const price = $('#chart_area > div.rate_info > div.today > p.no_today > em').text().replaceAll(/^\s+|\s+$/gm,'').split('\n')[0].replaceAll(',', '');
+    return Number(price);
+  } catch (error) {
+    console.error(error);
+    log.error('크롤링 에러');
+  }
+};
+
+const sendMsg = async () => {
+  let msg = '';
+  let logMsg = '';
+  for (let i = 0; i < data.length; i += 1) {
+    let total_my = 0;
+    let total_now = 0;
+    for (let j = 0; j < data[i].stocks.length; j += 1) {
+      data[i].stocks[j]['price'] = await getPresentPrice(data[i].stocks[j].code);
+      data[i].stocks[j]['total_now'] = data[i].stocks[j].price * data[i].stocks[j].cnt;
+      data[i].stocks[j]['total_my'] = data[i].stocks[j].average * data[i].stocks[j].cnt;
+      data[i].stocks[j]['total_diff'] = (data[i].stocks[j].price * data[i].stocks[j].cnt) - (data[i].stocks[j].average * data[i].stocks[j].cnt);
+      data[i].stocks[j]['total_per'] = Number((((data[i].stocks[j].price * data[i].stocks[j].cnt) - (data[i].stocks[j].average * data[i].stocks[j].cnt)) / (data[i].stocks[j].average * data[i].stocks[j].cnt) * 100).toFixed(2));
+
+      const stock = {...data[i].stocks[j]}
+      msg += `*[${stock.code}] ${stock.name}*\n
+평균매입가: ${priceToString(stock.average)} (${stock.cnt}주)\n
+현재가: ${priceToString(stock.price)}\n
+평가손익: ${priceToString(stock.total_diff)} *${stock.total_per}%*\n
+------------------------------------------------------------------\n`
+
+      total_my += stock.total_my;
+      total_now += stock.total_now;
+
+      if (j === data[i].stocks.length - 1) {
+        msg += `*${data[i].name}*\n
+총평가금액: ${priceToString(total_now)}\n
+총평가손익: ${priceToString((total_now - total_my))} *${Number(((total_now - total_my) / total_my * 100).toFixed(2))}%*\n
+------------------------------------------------------------------\n`;
+        logMsg += `${data[i].name}: ${Number(((total_now - total_my) / total_my * 100).toFixed(2))}% `;
+      }
+    }
+  }
+  try {
+    msg += '`' + moment().format(`YYYY-MM-DD ${days[moment().day()]}요일 HH:mm:ss`) + '`\n';
+    await web.chat.postMessage({
+      channel: slack.stock.channel,
+      text: msg,
+      as_user: true
+    });
+    log.info(logMsg);
+  } catch (error) {
+    console.log(error);
+    log.error('슬랙 메시지 전송 에러')
+  }
+}
+
+const getHistory = async () => {
+  const history = await web.conversations.history({
+    channel: slack.stock.channel,
+  });
+  return history;
+}
+const makeChart = async () => {
+  try {
+    const history = await getHistory();
+    const filterMessages = history.messages.filter((r) => r.text.indexOf('--------------------------------') != -1)
+      .filter((r, i) =>  i <= 40).map((r) => r.text).reverse();
+  
+    const samsung_aunt = [
+      {
+        label: '현재가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#1976D2', 
+        data: [],
+      },
+      {
+        label: '평균매입가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#000000',
+        data: []
+      },
+    ];
+    const samsung = [
+      {
+        label: '현재가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#1976D2', 
+        data: [],
+      },
+      {
+        label: '평균매입가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#000000',
+        data: []
+      },
+    ];
+    const nasdaq = [
+      {
+        label: '현재가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#1976D2', 
+        data: [],
+      },
+      {
+        label: '평균매입가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#000000',
+        data: []
+      },
+    ];
+    const snp = [
+      {
+        label: '현재가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#1976D2', 
+        data: [],
+      },
+      {
+        label: '평균매입가',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: '#000000',
+        data: []
+      },
+    ];
+    const date = [];
+  
+    let samsung_aunt_amount = '';
+    let samsung_amount = '';
+    let nasdaq_amount = '';
+    let snp_amount = '';
+  
+    const getNowPrice = (r, splitStr) => {
+      return Number(r.split(splitStr)[1].split('평가손익:')[0].split('현재가:')[1].split('원')[0].trim().replace(',', ''));
+    }
+    const getAmountPer = (r, splitStr) => {
+      return r.split(splitStr)[1].split('--------')[0].split('평가손익:')[1].split('----')[0].split('*')[1].trim().replaceAll('*', '');
+    }
+  
+    for (let i = 0; i < filterMessages.length; i += 1) {
+      const r = filterMessages[i];
+      const time = `${r.split('`')[1].slice(5, -6).replace('-', '/').replace('요일', '')}시`;
+      const now_aunt = getNowPrice(r, '삼성전자(고모)');
+      const now_samsung = getNowPrice(r, '삼성전자*');
+      const now_nasdaq = getNowPrice(r, '나스닥100*');
+      const now_snp = getNowPrice(r, 'P500*');
+  
+    
+  
+      if (i === filterMessages.length - 1) {
+        const amount_aunt = getAmountPer(r, '삼성전자(고모)');
+        const amount_samsung = getAmountPer(r, '삼성전자*');
+        const amount_nasdaq = getAmountPer(r, '나스닥100*');
+        const amount_snp = getAmountPer(r, 'P500*');
+        samsung_aunt_amount = amount_aunt;
+        samsung_amount = amount_samsung;
+        nasdaq_amount = amount_nasdaq;
+        snp_amount = amount_snp;
+      }
+  
+      date.push(time);
+  
+      samsung_aunt[1].data.push(data[0].stocks[1].average);
+      samsung_aunt[0].data.push(now_aunt);
+  
+      samsung[1].data.push(data[0].stocks[0].average);
+      samsung[0].data.push(now_samsung);
+  
+      nasdaq[1].data.push(data[1].stocks[0].average);
+      nasdaq[0].data.push(now_nasdaq);
+  
+      snp[1].data.push(data[1].stocks[1].average);
+      snp[0].data.push(now_snp);
+    }
+  
+    const aunt_chart = await ChartJSImage().chart({
+      type: 'line',
+      data: {
+        labels: date,
+        datasets: samsung_aunt,
+      },
+      options: {
+        title: {
+          display: true,
+          text: `삼성전자(고모) (현재 수익률: ${samsung_aunt_amount})`,
+        },
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ]
+        }
+      }
+    }).backgroundColor('white').width(700).height(500);
+    const samsung_chart = await ChartJSImage().chart({
+      type: 'line',
+      data: {
+        labels: date,
+        datasets: samsung,
+      },
+      options: {
+        title: {
+          display: true,
+          text: `삼성전자 (현재 수익률: ${samsung_amount})`,
+        },
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ]
+        }
+      }
+    }).backgroundColor('white').width(700).height(500);
+    const nasdaq_chart = await ChartJSImage().chart({
+      type: 'line',
+      data: {
+        labels: date,
+        datasets: nasdaq,
+      },
+      options: {
+        title: {
+          display: true,
+          text: `나스닥 (현재 수익률: ${nasdaq_amount})`,
+        },
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ]
+        }
+      }
+    }).backgroundColor('white').width(700).height(500);
+    const snp_chart = await ChartJSImage().chart({
+      type: 'line',
+      data: {
+        labels: date,
+        datasets: snp,
+      },
+      options: {
+        title: {
+          display: true,
+          text: `S&P500 (현재 수익률: ${snp_amount})`,
+        },
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+              }
+            }
+          ]
+        }
+      }
+    }).backgroundColor('white').width(700).height(500);
+  
+    aunt_chart.toFile('./chart/aunt.png');
+    samsung_chart.toFile('./chart/samung.png');
+    nasdaq_chart.toFile('./chart/nasdap.png');
+    snp_chart.toFile('./chart/snp.png');
+    log.info('MAKE CHART SUCCESS');
+  } catch (error) {
+    log.error('MAKE CHART ERROR');
+    console.log(error);
+  }
+}
+
+const sendChart = async () => {
+  try {
+    await web.files.upload({
+      channels: slack.stock.channel,
+      token: slack.stock.bot_token,
+      file: createReadStream('./chart/aunt.png'),
+      filename: 'aunt.png',
+    });
+    await web.files.upload({
+      channels: slack.stock.channel,
+      token: slack.stock.bot_token,
+      file: createReadStream('./chart/samung.png'),
+      filename: 'samung.png',
+    });
+    await web.files.upload({
+      channels: slack.stock.channel,
+      token: slack.stock.bot_token,
+      file: createReadStream('./chart/nasdap.png'),
+      filename: 'nasdap.png',
+    });
+    await web.files.upload({
+      channels: slack.stock.channel,
+      token: slack.stock.bot_token,
+      file: createReadStream('./chart/snp.png'),
+      filename: 'snp.png',
+    });
+    log.info('SEND CHART SUCCESS');
+  } catch (error) {
+    log.error('SEND CHART ERROR');
+    console.log(error);
+  }
+}
+
+const sendMsgWork = schedule.scheduleJob('0 0 9-16 * * MON-FRI', async () => {
+  sendMsg();
+});
+const makeChartWork = schedule.scheduleJob('0 1 16 * * FRI', async () => {
+  makeChart();
+});
+const sendChartWork = schedule.scheduleJob('0 2 16 * * FRI', async () => {
+  sendChart();
+});
