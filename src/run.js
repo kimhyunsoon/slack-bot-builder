f4dece04a36528e0466ca5bb9c4a2a3e9e723a8b
soon <sudosoon@gmail.com>
2022-09-07 11:19:38 +0900

    test func deleted

--- src/run.js
+++ src/run.js
@@ -1,20 +1,6 @@
 import cron from 'node-cron';
 import { sendLunch } from './work/lunch.js';
 import { sendStock, makeChart, sendChart } from './work/stock.js';
-import moment from 'moment';
-
-const days = ['일', '월', '화', '수', '목', '금', '토'];
-const log = {
-  error: async (msg) => {
-    let date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
-    console.error(`#error::${date} ${days[moment().day()]}요일 [ ${msg} ]`);
-  },
-  info: async (msg) => {
-    let date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
-    console.log(`#info::${date} ${days[moment().day()]}요일 [ ${msg} ]`);
-  }
-};
-
 
 const sendLunchWork = cron.schedule('0 0 11 * * MON-FRI', async () => {
   await sendLunch();
@@ -27,9 +13,4 @@ const makeChartWork = cron.schedule('0 1 16 * * FRI', async () => {
 });
 const sendChartWork = cron.schedule('0 2 16 * * FRI', async () => {
   await sendChart();
-});
-
-const test = cron.schedule('0 * 9-16 * * MON-FRI', async () => {
-  await log.info('test');
-  await sendStock();
 });
